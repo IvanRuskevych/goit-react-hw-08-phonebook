d596026e4a58db09eb4353f6cd0c9e23bb8dc178
unknown <ivan.ruskevych@gmail.com>
2023-06-25 05:51:26 +0300

    step-03 Routes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,7 @@
 export default function HomePage() {
   return (
     <div style={styles.container}>
-      <h1 style={styles.title}>
-        Task manager welcome page{' '}
-        <span role="img" aria-label="Greeting icon">
-          💁‍♀️
-        </span>
-      </h1>
+      <h1 style={styles.title}>Home Work 8 "Users Authentication"</h1>
     </div>
   );
 }
